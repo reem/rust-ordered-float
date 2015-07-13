738b78e17b226003118c357789b8db3e1cb8d6ba
Aaron Lindsay <aaron@aaronlindsay.com>
2015-07-13 00:40:08 -0400

    Extra trait impls and NotNaN encapsulation

--- tests/test.rs
+++ tests/test.rs
@@ -18,7 +18,7 @@ describe! ordered_float32 {
     it "should compare NaN" {
         let f32_nan: f32 = Float::nan();
         assert_eq!(OrderedFloat(f32_nan).cmp(&OrderedFloat(Float::nan())), Equal);
-        assert_eq!(OrderedFloat(Float::nan()).cmp(&OrderedFloat(-100000.0f32)), Greater);
+        assert_eq!(OrderedFloat(f32_nan).cmp(&OrderedFloat(-100000.0f32)), Greater);
         assert_eq!(OrderedFloat(-100.0f32).cmp(&OrderedFloat(Float::nan())), Less);
     }
 }
@@ -33,59 +33,33 @@ describe! ordered_float64 {
     it "should compare NaN" {
         let f64_nan: f64 = Float::nan();
         assert_eq!(OrderedFloat(f64_nan).cmp(&OrderedFloat(Float::nan())), Equal);
-        assert_eq!(OrderedFloat(Float::nan()).cmp(&OrderedFloat(-100000.0f64)), Greater);
+        assert_eq!(OrderedFloat(f64_nan).cmp(&OrderedFloat(-100000.0f64)), Greater);
         assert_eq!(OrderedFloat(-100.0f64).cmp(&OrderedFloat(Float::nan())), Less);
     }
 }
 
 describe! not_nan32 {
     it "should compare regular floats" {
-        assert_eq!(NotNaN(7.0f32).cmp(&NotNaN(7.0)), Equal);
-        assert_eq!(NotNaN(8.0f32).cmp(&NotNaN(7.0)), Greater);
-        assert_eq!(NotNaN(4.0f32).cmp(&NotNaN(7.0)), Less);
+        assert_eq!(NotNaN::from(7.0f32).cmp(&NotNaN::from(7.0)), Equal);
+        assert_eq!(NotNaN::from(8.0f32).cmp(&NotNaN::from(7.0)), Greater);
+        assert_eq!(NotNaN::from(4.0f32).cmp(&NotNaN::from(7.0)), Less);
     }
 
-    failing "should fail when comparing NaN to NaN" {
+    it "should fail when constructing NotNaN with NaN" {
         let f32_nan: f32 = Float::nan();
-        NotNaN(f32_nan).cmp(&NotNaN(Float::nan()));
-    }
-
-    failing "should fail when comparing NaN to a regular float" {
-        NotNaN(Float::nan()).cmp(&NotNaN(7.0f32));
-    }
-
-    failing "should fail when comparing a regular float to NaN" {
-        NotNaN(7.0f32).cmp(&NotNaN(Float::nan()));
-    }
-
-    failing "should fail when constructing NotNaN with NaN" {
-        let f32_nan: f32 = Float::nan();
-        NotNaN::new(f32_nan);
+        assert!(NotNaN::new(f32_nan).is_err());
     }
 }
 
 describe! not_nan64 {
     it "should compare regular floats" {
-        assert_eq!(NotNaN(7.0f64).cmp(&NotNaN(7.0)), Equal);
-        assert_eq!(NotNaN(8.0f64).cmp(&NotNaN(7.0)), Greater);
-        assert_eq!(NotNaN(4.0f64).cmp(&NotNaN(7.0)), Less);
+        assert_eq!(NotNaN::from(7.0f64).cmp(&NotNaN::from(7.0)), Equal);
+        assert_eq!(NotNaN::from(8.0f64).cmp(&NotNaN::from(7.0)), Greater);
+        assert_eq!(NotNaN::from(4.0f64).cmp(&NotNaN::from(7.0)), Less);
     }
 
-    failing "should panic when comparing NaN to NaN" {
+    it "should fail when constructing NotNaN with NaN" {
         let f64_nan: f64 = Float::nan();
-        NotNaN(f64_nan).cmp(&NotNaN(Float::nan()));
-    }
-
-    failing "should fail when comparing NaN to a regular float" {
-        NotNaN(Float::nan()).cmp(&NotNaN(7.0f64));
-    }
-
-    failing "should fail when comparing a regular float to NaN" {
-        NotNaN(7.0f64).cmp(&NotNaN(Float::nan()));
-    }
-
-    failing "should fail when constructing NotNaN with NaN" {
-        let f64_nan: f64 = Float::nan();
-        NotNaN::new(f64_nan);
+        assert!(NotNaN::new(f64_nan).is_err());
     }
 }
